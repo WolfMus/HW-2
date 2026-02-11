66bded112f418f8df90f9c65e1913c27abaedc98
MrSevere <mrsevere483@gmail.com>
2026-02-11 16:07:35 +0300

    added router for testing

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -3,18 +3,13 @@ import { db } from "./db/in-memory.db";
 import { HttpStatus } from "./core/types/types";
 import { blogsRouter } from "./blogs/routers/blogs.routers";
 import { postsRouters } from "./posts/routers/posts.routers";
-import { BLOGS_PATH, POSTS_PATH } from "./core/types/paths/paths";
+import { BLOGS_PATH, POSTS_PATH, TESTING_PATH } from "./core/types/paths/paths";
+import { testingRouter } from "./testing/testing.routers";
 
 
 export const setupApp = (app: Express) => {
   app.use(express.json());
-
-  app.delete("/testing/all-data", (req, res) => {
-    db.blogs = [];
-    db.posts = [];
-    res.sendStatus(HttpStatus.NoContent);
-  })
-
+  app.use(TESTING_PATH, testingRouter)
   app.use(BLOGS_PATH, blogsRouter);
   app.use(POSTS_PATH, postsRouters);
 
