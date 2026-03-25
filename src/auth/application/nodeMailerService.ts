7b7bafee7f9b7131691657ee8d7d73f753b3f353
MrSevere <mrsevere483@gmail.com>
2026-03-25 13:42:38 +0300

    fix

--- src/auth/application/nodeMailerService.ts
+++ src/auth/application/nodeMailerService.ts
@@ -6,32 +6,34 @@ export const nodeMailerService = {
   async sendEmail(userEmail: string, confirmationCode: string) {
     
     // REAL ACCOUNT
-    // const transporter = nodemailer.createTransport({
-    //   host: "smtp.gmail.com",
-    //   auth: {
-    //     user: "mrsevere484@gmail.com",
-    //     pass: process.env.GOOGLE_APP_PASSWORD,
-    //   },
-    // });
 
-
-    // MOCK ETHEREAL ACCOUNT  
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // Use true for port 465, false for port 587
+      host: "smtp.gmail.com",
       auth: {
-        user: "rhett88@ethereal.email",
-        pass: "RagH4VnZeBxkEP5een",
+        user: "mrsevere484@gmail.com",
+        pass: process.env.GOOGLE_APP_PASSWORD,
       },
     });
 
-    // try {
-    //   await transporter.verify();
-    //   console.log("Server is ready to take our messages");
-    // } catch (e) {
-    //   console.error("Verification failed", e);
-    // }
+
+    // MOCK ETHEREAL ACCOUNT  
+    
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false, // Use true for port 465, false for port 587
+    //   auth: {
+    //     user: "rhett88@ethereal.email",
+    //     pass: "RagH4VnZeBxkEP5een",
+    //   },
+    // });
+
+    try {
+      await transporter.verify();
+      console.log("Server is ready to take our messages");
+    } catch (e) {
+      console.error("Verification failed", e);
+    }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const info = await transporter.sendMail({
