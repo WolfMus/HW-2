db90cb0a24089db7ae527dc46b3677b33021b527
MrSevere <mrsevere483@gmail.com>
2026-04-15 00:13:55 +0300

    Introduce a 5-millisecond delay after initial create operation to simulate throttling and re-evaluate rate limits for better control.

--- src/auth/middleware/rate-limit.middleware.ts
+++ src/auth/middleware/rate-limit.middleware.ts
@@ -3,21 +3,34 @@ import { rateLimitRepository } from "../repositories/rate-limit.repository";
 import { HttpStatus } from "../../core/types/types";
 import { subSeconds } from "date-fns";
 
-export const rateLimitMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+export const rateLimitMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const url = req.originalUrl;
+  const ip = req.ip!;
+  const tenSecondsAgo = subSeconds(new Date(), 10);
 
-    const url = req.originalUrl;
-    const ip = req.ip!;
-    const tenSecondsAgo = subSeconds(new Date(), 10);
+  await rateLimitRepository.deleteOld(ip, url, tenSecondsAgo);
 
-    await rateLimitRepository.deleteOld(ip, url, tenSecondsAgo);
+  let amountOfCalls = await rateLimitRepository.find(ip, url, tenSecondsAgo);
+  console.log(amountOfCalls);
+  if (amountOfCalls >= 5) {
+    res.sendStatus(HttpStatus.TooManyRequests);
+    return;
+  }
+  await rateLimitRepository.create(ip, url);
 
-    const amountOfCalls = await rateLimitRepository.find(ip, url, tenSecondsAgo);
-    console.log(amountOfCalls)
-    if (amountOfCalls >= 5) {
-        res.sendStatus(HttpStatus.TooManyRequests);
-        return;
-    } 
-    await rateLimitRepository.create(ip, url)
-    next()
+  await new Promise((resolve) => setTimeout(resolve, 5));
 
-}
+  amountOfCalls = await rateLimitRepository.find(ip, url, tenSecondsAgo);
+  console.log(amountOfCalls);
+  if (amountOfCalls >= 5) {
+    res.sendStatus(HttpStatus.TooManyRequests);
+    return;
+  }
+  await rateLimitRepository.create(ip, url);
+
+  next();
+};
