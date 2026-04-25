e06ed1c0649b72c54488c11ba9227888de861e8d
MrSevere <mrsevere483@gmail.com>
2026-04-25 15:29:16 +0300

    switch from mock acc to real

--- src/auth/application/nodeMailerService.ts
+++ src/auth/application/nodeMailerService.ts
@@ -5,24 +5,24 @@ dotenv.config();
 export class NodeMailerService {
   async sendEmail(userEmail: string, confirmationCode: string) {
     // REAL ACCOUNT
-    // const transporter = nodemailer.createTransport({
-    //   host: "smtp.gmail.com",
-    //   auth: {
-    //     user: "mrsevere484@gmail.com",
-    //     pass: process.env.GOOGLE_APP_PASSWORD,
-    //   },
-    // });
-
-    // MOCK ETHEREAL ACCOUNT
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // Use true for port 465, false for port 587
+      host: "smtp.gmail.com",
       auth: {
-        user: "rhett88@ethereal.email",
-        pass: "RagH4VnZeBxkEP5een",
+        user: "mrsevere484@gmail.com",
+        pass: process.env.GOOGLE_APP_PASSWORD,
       },
     });
+
+    // MOCK ETHEREAL ACCOUNT
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false, // Use true for port 465, false for port 587
+    //   auth: {
+    //     user: "rhett88@ethereal.email",
+    //     pass: "RagH4VnZeBxkEP5een",
+    //   },
+    // });
 
     try {
       await transporter.verify();
@@ -33,23 +33,23 @@ export class NodeMailerService {
 
     // REAL ACCOUNT
     //eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // const info = await transporter.sendMail({
-    //   from: 'mrsevere484@gmail.com',
-    //   to: userEmail,
-    //   subject: "Registration",
-    //   text: "Welcome", // Plain-text version of the message
-    //   html: `<h1>Thank for your registration</h1><p>To finish registration please follow the link below:<a href='http://localhost:5003/auth/registration-confirmation?code=${confirmationCode}'>complete registration</a></p>`,
-    // });
-
-    // MOCK ETHEREAL ACCOUNT
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const info = await transporter.sendMail({
-      from: 'rhett88@ethereal.email',
+      from: 'mrsevere484@gmail.com',
       to: userEmail,
       subject: "Registration",
       text: "Welcome", // Plain-text version of the message
       html: `<h1>Thank for your registration</h1><p>To finish registration please follow the link below:<a href='http://localhost:5003/auth/registration-confirmation?code=${confirmationCode}'>complete registration</a></p>`,
     });
+
+    // MOCK ETHEREAL ACCOUNT
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const info = await transporter.sendMail({
+    //   from: 'rhett88@ethereal.email',
+    //   to: userEmail,
+    //   subject: "Registration",
+    //   text: "Welcome", // Plain-text version of the message
+    //   html: `<h1>Thank for your registration</h1><p>To finish registration please follow the link below:<a href='http://localhost:5003/auth/registration-confirmation?code=${confirmationCode}'>complete registration</a></p>`,
+    // });
 
     console.log("QUERY: ", confirmationCode);
   }
@@ -57,24 +57,24 @@ export class NodeMailerService {
   async sendRecoveryCode(userEmail: string, recoveryCode: string) {
        
     // REAL ACCOUNT
-    // const transporter = nodemailer.createTransport({
-    //   host: "smtp.gmail.com",
-    //   auth: {
-    //     user: "mrsevere484@gmail.com",
-    //     pass: process.env.GOOGLE_APP_PASSWORD,
-    //   },
-    // });
-
-    // MOCK ETHEREAL ACCOUNT  
     const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      port: 587,
-      secure: false, // Use true for port 465, false for port 587
+      host: "smtp.gmail.com",
       auth: {
-        user: "rhett88@ethereal.email",
-        pass: "RagH4VnZeBxkEP5een",
+        user: "mrsevere484@gmail.com",
+        pass: process.env.GOOGLE_APP_PASSWORD,
       },
     });
+
+    // MOCK ETHEREAL ACCOUNT  
+    // const transporter = nodemailer.createTransport({
+    //   host: "smtp.ethereal.email",
+    //   port: 587,
+    //   secure: false, // Use true for port 465, false for port 587
+    //   auth: {
+    //     user: "rhett88@ethereal.email",
+    //     pass: "RagH4VnZeBxkEP5een",
+    //   },
+    // });
 
     try {
       await transporter.verify();
@@ -85,23 +85,23 @@ export class NodeMailerService {
 
     // REAL ACCOUNT
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // const info = await transporter.sendMail({
-    //   from: 'mrsevere484@gmail.com',
-    //   to: userEmail,
-    //   subject: "Registration",
-    //   text: "Welcome", // Plain-text version of the message
-    //   html: `<h1>Password recovery</h1><p>To finish password recovery please follow the link below:<a href='http://localhost:5003/auth/registration-confirmation?code=${recoveryCode}'>recovery password</a></p>`,
-    // });
-
-    // MOCK ETHEREAL ACCOUNT
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const info = await transporter.sendMail({
-      from: 'rhett88@ethereal.email',
+      from: 'mrsevere484@gmail.com',
       to: userEmail,
       subject: "Registration",
       text: "Welcome", // Plain-text version of the message
-      html: `<h1>Password recovery</h1><p>To finish password recovery please follow the link below:<a href='http://localhost:5003/auth/password-recovery?recoveryCode=${recoveryCode}'>recovery password</a></p>`,
+      html: `<h1>Password recovery</h1><p>To finish password recovery please follow the link below:<a href='http://localhost:5003/auth/registration-confirmation?code=${recoveryCode}'>recovery password</a></p>`,
     });
+
+    // MOCK ETHEREAL ACCOUNT
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const info = await transporter.sendMail({
+    //   from: 'rhett88@ethereal.email',
+    //   to: userEmail,
+    //   subject: "Registration",
+    //   text: "Welcome", // Plain-text version of the message
+    //   html: `<h1>Password recovery</h1><p>To finish password recovery please follow the link below:<a href='http://localhost:5003/auth/password-recovery?recoveryCode=${recoveryCode}'>recovery password</a></p>`,
+    // });
 
     console.log("recoveryCode: ", recoveryCode);
   }
